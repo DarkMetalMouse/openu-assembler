e79adeec3cb737daa53ea08b2216b76d51cbbeae
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-03-15 02:24:41 +0200

    finished instruction data type

--- instruction_list.c
+++ instruction_list.c
@@ -1,136 +1,371 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
-typedef enum opcode {
-    mov= 1<<0,
-    cmp=1<<1,
-    add=1<<2,
-    sub=1<<2,
+typedef enum opcode
+{
+    mov,
+    cmp,
+    add,
+    sub,
 
-    lea=1<<4,
-    clr=1<<5,
-    not=1<<5,
-    inc=1<<5,
+    lea,
+    clr,
+    not,
+    inc,
 
-    dec=1<<5,
-    jmp=1<<9,
-    bne=1<<9,
-    jsr=1<<9,
+    dec,
+    jmp,
+    bne,
+    jsr,
 
-    red=1<<12,
-    prn=1<<13,
-    rts=1<<14,
-    stop=1<<15
+    red,
+    prn,
+    rts,
+    stop
 } opcode;
 
-typedef enum funct {
-    mov_funct=0,
-    cmp_funct=0,
-    add_funct=10,
-    sub_funct=11,
+const int OPCODE_VALUE[] =
+    {
+        0,
+        1,
+        2,
+        2,
 
-    lea_funct=0,
-    clr_funct=10,
-    not_funct=11,
-    inc_funct=12,
+        4,
+        5,
+        5,
+        5,
 
-    dec_funct=13,
-    jmp_funct=10,
-    bne_funct=11,
-    jsr_funct=12,
+        5,
+        9,
+        9,
+        9,
 
-    red_funct=0,
-    prn_funct=0,
-    rts_funct=0,
-    stop_funct=0
-} funct;
+        12,
+        13,
+        14,
+        15};
 
-typedef enum ARE {
-    A=1<<2,
-    R=1<<1,
-    E=1<<0
+const int OPCODE_FUNCT[] =
+    {
+        0,
+        0,
+        10,
+        11,
+
+        0,
+        10,
+        11,
+        12,
+
+        13,
+        10,
+        11,
+        12,
+
+        0,
+        0,
+        0,
+        0};
+
+typedef enum ARE
+{
+    absolute = 1 << 2,
+    relocatable = 1 << 1,
+    external = 1 << 0
 } ARE;
 
-typedef union line
+typedef enum operand_type
+{
+    immidiate,
+    direct,
+    index,
+    reg
+} operand_type;
+
+const int OPERAND_SIZE[] = {1, 2, 2, 0};
+
+typedef union word
 {
     struct format
     {
-        unsigned int data : 16;
-        unsigned int ARE:3;
-        unsigned int _1:1;
+        union data
+        {
+            struct __attribute__((__packed__)) funct_word
+            {
+                unsigned int dst_type : 2;
+                unsigned int dst_reg : 4;
+                unsigned int src_type : 2;
+                unsigned int src_reg : 4;
+                unsigned int funct : 4;
+            } funct_word;
+            uint16_t raw : 16;
+
+        } data;
+        unsigned int ARE : 3;
+        unsigned int _1 : 1;
     } format;
     unsigned int raw : 20;
-} line;
-
+} word;
 
 typedef struct instruction
 {
-    line *lines;
+    word *words;
     int size;
+    int filled;
 } instruction;
 
 typedef struct operand
 {
-    union data {
+    union data_type
+    {
         uint16_t imm;
-        uint16_t address;
+
+        struct address
+        {
+            uint16_t value;
+            unsigned int is_external : 1;
+        } address;
+
         struct index
         {
-            uint16_t address;
+            struct address address;
             unsigned int reg : 4;
         } index;
+
         unsigned int reg : 4;
-    } data;
-    unsigned int type : 2; 
+    } data_type;
+    operand_type type;
+} operand;
 
-} openrand;
-
-
-
-instruction *i_create_no_operand(opcode opcode) {
-    instruction *i = NULL;
-    switch (opcode)
-    {
-    case rts:
-    case stop:
-        
-        i = i_create_n(1);
-        i->lines[0].format.data = opcode;
-        break;
-    default:
-        /* ERROR NOT ENOUGH ARGUMENTS */
-        break;
-    }
-    return i;
+int get_operand_size(operand operand)
+{
+    return OPERAND_SIZE[operand.type];
 }
 
-instruction *i_create_one_operand(opcode opcode) {
-    instruction *i = NULL;
-    switch (opcode)
-    {
-    case clr | not | inc | dec: /* They have the same value, it will be optimized*/
-    case jmp | bne | jsr:
-    case red:
-    case prn:
-    case stop:
-        i = i_create_n(1);
-        i->lines[0].format.data = opcode;
-        break;
-    default:
-        /* ERROR NOT ENOUGH ARGUMENTS */
-        break;
-    }
-    return i;
+int within(int val, int min, int max)
+{
+    return min <= val && val <= max;
 }
 
-instruction *i_create_n(int size) {
-    instruction *i = malloc(sizeof(instruction));
-    i->size = size;
-    i->lines = malloc(sizeof(line) * size);
-    return i;
+int is_n_operands(opcode opcode, int n)
+{
+    switch (n)
+    {
+    /* all n operand opcodes are within unique ranges */
+    case 0:
+        return within(OPCODE_VALUE[opcode], rts, stop);
+    case 1:
+        return within(OPCODE_VALUE[opcode], clr, prn);
+    case 2:
+        return within(OPCODE_VALUE[opcode], mov, lea);
+    default:
+        return 0;
+    }
+}
+
+instruction *i_allocate(int size)
+{
+    instruction *inst = malloc(sizeof(instruction));
+    inst->size = size;
+    inst->filled = 0;
+    inst->words = malloc(sizeof(word) * size);
+    return inst;
+}
+
+void i_fill(instruction *inst, uint16_t data, ARE attribute)
+{
+    inst->words[inst->filled].format.ARE = attribute;
+    inst->words[inst->filled++].format.data.raw = data;
+}
+
+void i_write_operand(instruction *inst, operand operand, int is_dest)
+{
+    operand_type type = operand.type;
+    ARE attribute;
+    if (is_dest)
+    {
+        inst->words[1].format.data.funct_word.dst_type = type & 3; /* & 3 to make sure we only write 2 bits */
+    }
+    else
+    {
+        inst->words[1].format.data.funct_word.src_type = type & 3; /* & 3 to make sure we only write 2 bits */
+    }
+
+    switch (type)
+    {
+    case immidiate:
+        i_fill(inst, operand.data_type.imm, absolute);
+
+        break;
+    case direct:
+        attribute = operand.data_type.address.is_external ? external : relocatable;
+        i_fill(inst, operand.data_type.address.value & 0xF0, attribute);
+        i_fill(inst, operand.data_type.address.value & 0x0F, attribute);
+        break;
+    case index:
+        attribute = operand.data_type.index.address.is_external ? external : relocatable;
+        i_fill(inst, operand.data_type.index.address.value & 0xF0, attribute);
+        i_fill(inst, operand.data_type.index.address.value & 0x0F, attribute);
+        if (is_dest)
+        {
+            inst->words[1].format.data.funct_word.dst_reg = operand.data_type.index.reg;
+        }
+        else
+        {
+            inst->words[1].format.data.funct_word.src_reg = operand.data_type.index.reg;
+        }
+        break;
+    case reg:
+        if (is_dest)
+        {
+            inst->words[1].format.data.funct_word.dst_reg = operand.data_type.reg;
+        }
+        else
+        {
+            inst->words[1].format.data.funct_word.src_reg = operand.data_type.reg;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+instruction *i_create(opcode opcode, int operand_count, operand operands[])
+{
+    instruction *inst = NULL;
+    int size = 1; /* minimum instruction size */
+    int i;
+    if (!is_n_operands(opcode, operand_count))
+    {
+        /* ERROR OPERAND COUNT MISMATCH */
+        return inst;
+    }
+
+    size += operand_count ? 1 : 0; /* funct word space only applicable for instructions with operands*/
+    for (i = 0; i < operand_count; i++)
+    {
+        size += get_operand_size(operands[i]);
+    }
+    inst = i_allocate(size);
+
+    i_fill(inst, 1 << OPCODE_VALUE[opcode], absolute);
+    inst->filled++; /* skip funct for now */
+
+    switch (operand_count)
+    {
+    case 2: /* handle source operand */
+
+        i_write_operand(inst, operands[0], 0);
+
+        /* fallthrough */
+    case 1: /* handle destination operand */
+
+        i_write_operand(inst, operands[operand_count - 1], 1); /* operand_count - 1 : 2 operands -> index 1; 1 operand -> index 0*/
+        inst->words[1].format.data.funct_word.funct = OPCODE_FUNCT[opcode];
+        inst->words[1].format.ARE = absolute;
+
+        /* fallthrough */
+    default: /* case 0 */
+
+        break;
+    }
+    return inst;
+}
+
+void i_print(instruction *inst)
+{
+    int i;
+    for (i = 0; i < inst->size; i++)
+    {
+        printf("%x\n", (uint32_t)inst->words[i].raw);
+    }
+    printf("---\n");
 }
 
 int main(int argc, char const *argv[])
 {
+    instruction *i;
+
+    i = i_create(add, 2, (operand[]){
+                             {.data_type.reg = 3, .type = reg},   //
+                             {.data_type.address.is_external = 0, //
+                              .data_type.address.value = 146,     //
+                              .type = direct}                     //
+                         });
+    i_print(i);
+
+    i = i_create(prn, 1, (operand[]){{.data_type.imm = 48, .type = immidiate}});
+    i_print(i);
+
+    i = i_create(lea, 2, (operand[]){
+                             {.data_type.address.is_external = 0, //
+                              .data_type.address.value = 141,     //
+                              .type = direct},                    //
+                             {.data_type.reg = 6, .type = reg}    //
+                         });
+    i_print(i);
+
+    i = i_create(inc, 1, (operand[]){{.data_type.reg = 6, .type = reg}});
+    i_print(i);
+
+    i = i_create(mov, 2, (operand[]){
+                             {.data_type.reg = 3, .type = reg},   //
+                             {.data_type.address.is_external = 1, //
+                              .data_type.address.value = 0,       //
+                              .type = direct}                     //
+                         });
+    i_print(i);
+
+    i = i_create(sub, 2, (operand[]){
+                             {.data_type.reg = 1, .type = reg}, //
+                             {.data_type.reg = 4, .type = reg}  //
+                         });
+    i_print(i);
+
+    i = i_create(bne, 1, (operand[]){
+                             {.data_type.address.is_external = 0, //
+                              .data_type.address.value = 140,     //
+                              .type = direct}                     //
+                         });
+    i_print(i);
+
+    i = i_create(cmp, 2, (operand[]){
+                             {.data_type.address.is_external = 1,     //
+                              .data_type.address.value = 0,           //
+                              .type = direct},                        //
+                             {.data_type.imm = -6, .type = immidiate} //
+                         });
+    i_print(i);
+
+    i = i_create(bne, 1, (operand[]){
+                             {.data_type.index.address.is_external = 0, //
+                              .data_type.index.address.value = 140,     //
+                              .data_type.index.reg = 15,                //
+                              .type = index},                           //
+                         });
+    i_print(i);
+
+    i = i_create(dec, 1, (operand[]){
+                             {.data_type.address.is_external = 0, //
+                              .data_type.address.value = 149,     //
+                              .type = direct}                     //
+                         });
+    i_print(i);
+
+    i = i_create(sub, 2, (operand[]){
+                             {.data_type.index.address.is_external = 0, //
+                              .data_type.index.address.value = 104,     //
+                              .data_type.index.reg = 10,                //
+                              .type = index},                           //
+                             {.data_type.reg = 14, .type = reg}         //
+                         });
+    i_print(i);
+
+    i = i_create(stop, 0, (operand[]){});
+    i_print(i);
+
     return 0;
 }
